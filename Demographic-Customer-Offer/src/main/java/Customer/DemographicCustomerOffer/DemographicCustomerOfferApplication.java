953965bc988697ef4c3143647a5606973c20a21f
sumanth kollipara <sumanthkumar44@gmail.com>
2023-07-27 19:31:25 +0100

    Add files via upload
    initial commit

---
+++ Demographic-Customer-Offer/src/main/java/Customer/DemographicCustomerOffer/DemographicCustomerOfferApplication.java
@@ -0,0 +1,13 @@
+package Customer.DemographicCustomerOffer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemographicCustomerOfferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemographicCustomerOfferApplication.class, args);
+	}
+
+}
---
+++ Demographic-Customer-Offer/src/test/java/Customer/DemographicCustomerOffer/DemographicCustomerOfferApplicationTests.java
@@ -0,0 +1,13 @@
+package Customer.DemographicCustomerOffer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemographicCustomerOfferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
