8e6706bfcf00d36450ee1a4ab923a46e14861414
sumanth kollipara <sumanthkumar44@gmail.com>
2023-07-27 19:36:56 +0100

    Add files via upload
    initial commin

---
+++ Demographic-offers/src/main/java/Customer/Demographicoffers/DemographicOffersApplication.java
@@ -0,0 +1,13 @@
+package Customer.Demographicoffers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemographicOffersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemographicOffersApplication.class, args);
+	}
+
+}
---
+++ Demographic-offers/src/test/java/Customer/Demographicoffers/DemographicOffersApplicationTests.java
@@ -0,0 +1,13 @@
+package Customer.Demographicoffers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemographicOffersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
